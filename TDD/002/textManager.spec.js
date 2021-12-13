3ad8b9f4735965dfe55049a64b809e7e778e0617
nurimeansworld <nurimeansworld@gmail.com>
2021-12-13 11:25:11 +0900

    REFACT-beforEach 활용 #13

--- TDD/002/textManager.spec.js
+++ TDD/002/textManager.spec.js
@@ -1,12 +1,14 @@
 describe("텍스트 관리자입니다.", () => {
+  let textManager;
+  beforeEach(() => {
+    textManager = new TextManager();
+  });
   it("텍스트 값을 전달합니다.", () => {
-    const textManager = new TextManager();
     const initValue = textManager.getValue();
     expect(textManager.getValue()).toBe(initValue);
   });
 
   it("텍스트 값을 수정합니다.", () => {
-    const textManager = new TextManager();
     const newText = { data: "Hello Zebras" };
     textManager.setValue(newText);
 
