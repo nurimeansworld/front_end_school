96aa025a9e6b3b7aaa46d8437be2832758efbef3
nurimeansworld <nurimeansworld@gmail.com>
2022-01-07 17:26:13 +0900

    Add: npx create react app으로 초기 생성 #19

---
+++ react/my-app/src/App.js
@@ -0,0 +1,21 @@
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>App.js test1</h1>
+      <p>App.js test2</p>
+    </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <p>Edit <code>src/App.js</code> and save to reload.</p>
+    //     <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
