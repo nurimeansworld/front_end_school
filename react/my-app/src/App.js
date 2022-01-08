566bd12682ea3f585774202ccc04a0cbcd084e8d
nurimeansworld <nurimeansworld@gmail.com>
2022-01-08 16:22:42 +0900

    Add: component props 활용 #19

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 import React, { fragment } from 'react';  // 5. fragment 활용
 
 // 6. component화
-function Licat() {
+function Licat(props) {
   // 4. js를 적용해보자
   const text = '이건 js를 활용해 넣은 텍스트!';
 
   return (
     <>
       {/* 1. return에 최상위태그 div로 감싸는 컴포넌트 생성해보기 */}
-			<h1>안녕, 라이캣 1호</h1>
-			<h1>안녕, 라이캣 2호!</h1>
+			<h1>안녕, {props.name} 1호</h1>
+			<h1>안녕, {props.name} 2호!</h1>
 
       {/* 2. class가 추가된 코드 넣어보기. class가 아닌 className임을 유의! */}
       <div className="newClass">class를 추가해본다.</div>
@@ -40,7 +40,7 @@ function Today() {
 function App() {
   return (
     <>
-      <Licat />
+      <Licat name="개리"/>
       <Today />
     </>
   );
