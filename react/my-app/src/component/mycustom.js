3fba962005416d59d522ce0b375fe0a0e9137979
nurimeansworld <nurimeansworld@gmail.com>
2022-01-08 17:37:18 +0900

    Add: hook-useState 실습 #19

--- react/my-app/src/component/mycustom.js
+++ react/my-app/src/component/mycustom.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState }from 'react';
+// useState를 이용해 전체 렌더링이 아닌 컴포넌트 렌더링을 하여 더 효율적으로 실행
 
 function Mycustom(props) {
+  const [like, setLike] = useState(0);
+  // console.log(useState(0));
+
+  function addLike() {
+    setLike(like + 1);
+  }
+  function hideLike() {
+    setLike(0);
+  }
 
   return(
     <>
       <p>이름 : {props.name}</p>
       <p>색상 : {props.color}</p>
       <p>좋아하는 음식 : {props.food}</p>
+
+      <button onClick={addLike}>좋아요++ <span>{like}</span></button>
+      <button onClick={hideLike}>좋아요 초기화 <span>{like}</span></button>
     </>
   );
 }
