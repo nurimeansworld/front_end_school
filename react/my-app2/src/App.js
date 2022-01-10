78cc3941964786889f95344885b4bc1327c10492
nurimeansworld <nurimeansworld@gmail.com>
2022-01-10 13:17:33 +0900

    Add: 8. 컴포넌트 조건 개념 #21

--- react/my-app2/src/App.js
+++ react/my-app2/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 
+function Noname() {
+  return(
+    <>
+      <p>name이 입력되지 않았습니다.</p>
+    </>
+  )
+}
+
 function Hello(props){
-  // const name = props.name;
-  // const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  // const numComponentsArray = num.map((i) => (
-  //   <h1 key={i.toString()}>
-  //     안녕~ {name} {i}호~
-  //   </h1>
-  // ));
+  const name = props.name;
+  const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  
+  const numComponentsArray = num.map((i) => (
+    <h1 key={i.toString()}>
+      안녕~ {name} {i}호~
+    </h1>
+  ));
 
   const productList = {
     products: [
@@ -40,22 +49,26 @@ function Hello(props){
     </div>
   ));
 
-  return(
-    // <>
-    //   <h1>안녕~ {name} 1호~</h1>
-    //   <h1>안녕~ {name} 2호~</h1>
-    //   <h1>안녕~ {name} 3호~</h1>
-    // </>
-    <>
-      {/* {numComponentsArray} */}
-      {resultProductList}
-    </>
-  )
+  // if문으로 return을 각각 설정하여 조건처리
+  if(name){
+    return(
+      <>
+        {numComponentsArray}
+      </>
+    )
+  }else{
+    return(
+      <>
+        <Noname />
+      </>
+    )
+  }
 }
+
 
 function App() {
   return (
-   <Hello name="licat"/>
+   <Hello name=""/>
   );
 }
 
