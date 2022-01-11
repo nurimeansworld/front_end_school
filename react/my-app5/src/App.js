9fa95bad39ea6e0a6761fbb750d5117ffa02d1a3
nurimeansworld <nurimeansworld@gmail.com>
2022-01-11 16:20:51 +0900

    Add: useEffect 예제 #21

--- react/my-app5/src/App.js
+++ react/my-app5/src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
-import React, { useState } from 'react';
+// 1. useEffect import
+import React, { useState, useEffect } from 'react'; 
 
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [render, setRender] = useState(false);
   const countUp = (e) => {
     setCount(count+1)
   }
+
+  useEffect(() => {
+    // 2. useEffect 설정하여 감지할 동작 설정
+    if(render){
+      if(count%2){
+        console.log('홀수!');
+      }else{
+        console.log('짝수!');
+      }
+    }
+    setRender(true)
+  }, [count])
 
   return(
     <>
