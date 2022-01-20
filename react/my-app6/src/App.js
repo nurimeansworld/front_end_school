33cf715098a07316a65b560b1db9adad464241a1
nurimeansworld <nurimeansworld@gmail.com>
2022-01-20 13:16:04 +0900

    Edit: ContextAPI-2 props로 자식에게 값 전달 #23

--- react/my-app6/src/App.js
+++ react/my-app6/src/App.js
@@ -1,6 +1,15 @@
 // import './App.css';
 import React from "react";
 
+const HelloNuri2 = (props) => {
+
+  return(
+    <div>
+      <h2>Two : {props.value.id}</h2>
+      <strong>Two : {props.value.name}</strong>
+    </div>
+  );
+}
 const HelloNuri = (props) => {
   const id = props.value.id;
   const name = props.value.name;
@@ -9,6 +18,7 @@ const HelloNuri = (props) => {
     <div>
       <h2>id : {id}</h2>
       <p>name : {name}</p>
+      <HelloNuri2 value={{id, name}}/>
     </div>
   );
 }
