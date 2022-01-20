9e3194aadfef41a8038036a8f5dc46b8cebd35ba
nurimeansworld <nurimeansworld@gmail.com>
2022-01-20 13:10:10 +0900

    Edit: ContextAPI-1 props로 값 전달 #23

--- react/my-app6/src/App.js
+++ react/my-app6/src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+// import './App.css';
+import React from "react";
+
+const HelloNuri = (props) => {
+  const id = props.value.id;
+  const name = props.value.name;
+
+  return(
+    <div>
+      <h2>id : {id}</h2>
+      <p>name : {name}</p>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <HelloNuri value={{name: 'nuri', id: 'nuriisfree'}}/>
+    </>
   );
 }
 
