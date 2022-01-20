b0b3c335275a11a3d513c7ce817e4e53dd324558
nurimeansworld <nurimeansworld@gmail.com>
2022-01-20 14:12:27 +0900

    Edit: Hook-5 useContext #23

--- react/my-app6/src/App.js
+++ react/my-app6/src/App.js
@@ -1,49 +1,62 @@
 // import './App.css';
-import React, { createContext } from "react";
+import React, { createContext, useContext } from "react";
 
 const userInfo = createContext({id: 'nuriisfree', name: 'nuri'});
 
-const HelloNuri2 = (props) => {
-
+const HelloNuri2 = () => {
+  const {id, name} = useContext(userInfo);
   return(
     <div>
-      <h2>2. id : {props.value.id}</h2>
-      <strong>2. name : {props.value.name}</strong>
+      <h2>2. id : {id}</h2>
+      <strong>2. name : {name}</strong>
     </div>
   );
 }
-const HelloNuri = (props) => {
+const HelloNuri = () => {
   // const id = props.value.id;
   // const name = props.value.name;
+  const {id, name} = useContext(userInfo);
 
   return(
-    <userInfo.Consumer>
-      {(value) => (
-        <div>
-          <h2>1. id : {value.id}</h2>
-          <p>2. name : {value.name}</p>
-          <HelloNuri2 value={value} />
-        </div>
-      )}
-    </userInfo.Consumer>
+    // <userInfo.Consumer>
+    //   {(value) => (
+    //     <div>
+    //       <h2>1. id : {value.id}</h2>
+    //       <p>2. name : {value.name}</p>
+    //       <HelloNuri2 value={value} />
+    //     </div>
+    //   )}
+    // </userInfo.Consumer>
+    <div>
+      <h2>1. id : {id}</h2>
+      <p>1. name : {name}</p>
+      <HelloNuri2/>
+    </div>
   );
 }
 
 function App() {
   return (
+    // 1. 그냥 props로 값 전달하기
     // <>
     //   <HelloNuri value={{id: 'nuriisfree', name: 'nuri'}}/>
     // </>
-    // 이렇게 createContext한 변수에 .consumer 메소드를 이용해 값을 받아와 진행
-   <>
-    <userInfo.Consumer>
-      {(value) => (<HelloNuri value={value} />)}
-    </userInfo.Consumer>
-    
-    <userInfo.Provider value={{ name: "ali", id: "nodeMaster" }}>
+
+    // 2. createContext : 이렇게 createContext한 변수에 .consumer 메소드를 이용해 값을 받아와 진행
+    //  <>
+    //   <userInfo.Consumer>
+    //     {(value) => (<HelloNuri value={value} />)}
+    //   </userInfo.Consumer>
+      
+    //   <userInfo.Provider value={{ name: "ali", id: "nodeMaster" }}>
+    //     <HelloNuri/>
+    //   </userInfo.Provider>
+    // </> 
+
+    // 3. useContext : Hook 이용하기
+    <>
       <HelloNuri/>
-    </userInfo.Provider>
-  </> 
+    </>
   );
 }
 
