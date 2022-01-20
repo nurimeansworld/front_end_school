4a74d123c3f4c6d40c7ad04d75071d92c3d2d1c7
nurimeansworld <nurimeansworld@gmail.com>
2022-01-20 13:35:38 +0900

    Edit: ContextAPI-4 createContext 사용(Context.Provider) #23

--- react/my-app6/src/App.js
+++ react/my-app6/src/App.js
@@ -7,21 +7,25 @@ const HelloNuri2 = (props) => {
 
   return(
     <div>
-      <h2>Two : {props.value.id}</h2>
-      <strong>Two : {props.value.name}</strong>
+      <h2>2. id : {props.value.id}</h2>
+      <strong>2. name : {props.value.name}</strong>
     </div>
   );
 }
 const HelloNuri = (props) => {
-  const id = props.value.id;
-  const name = props.value.name;
+  // const id = props.value.id;
+  // const name = props.value.name;
 
   return(
-    <div>
-      <h2>id : {id}</h2>
-      <p>name : {name}</p>
-      <HelloNuri2 value={{id, name}}/>
-    </div>
+    <userInfo.Consumer>
+      {(value) => (
+        <div>
+          <h2>1. id : {value.id}</h2>
+          <p>2. name : {value.name}</p>
+          <HelloNuri2 value={value} />
+        </div>
+      )}
+    </userInfo.Consumer>
   );
 }
 
@@ -31,9 +35,15 @@ function App() {
     //   <HelloNuri value={{id: 'nuriisfree', name: 'nuri'}}/>
     // </>
     // 이렇게 createContext한 변수에 .consumer 메소드를 이용해 값을 받아와 진행
+   <>
     <userInfo.Consumer>
       {(value) => (<HelloNuri value={value} />)}
     </userInfo.Consumer>
+    
+    <userInfo.Provider value={{ name: "ali", id: "nodeMaster" }}>
+      <HelloNuri/>
+    </userInfo.Provider>
+  </> 
   );
 }
 
